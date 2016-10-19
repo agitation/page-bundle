7539c23e91bc09ba3126ae762ad4a2547b61e203
Alexander Günsche <mail@lxg.de>
2016-10-19 20:31:20 +0200

    restructure Twig functions

--- lib/Service/NavigationExtension.php
+++ lib/Service/NavigationExtension.php
@@ -7,35 +7,54 @@
  * @license    http://opensource.org/licenses/MIT
  */
 
-namespace Agit\PageBundle\Twig;
+namespace Agit\PageBundle\Service;
 
-use Agit\PageBundle\Service\PageService;
 use Twig_Extension;
 use Twig_SimpleFunction;
+use Agit\IntlBundle\Service\LocaleConfigService;
+use Agit\IntlBundle\Service\LocaleService;
+use Agit\LocaleDataBundle\Entity\LanguageRepository;
+use Agit\PageBundle\Service\PageService;
+use Collator;
 
-class PageNavigationExtension extends Twig_Extension
+class NavigationExtension extends Twig_Extension
 {
-    private $pageService;
-
     private $pages;
 
     private $cache = [];
 
-    public function __construct(PageService $pageService)
+    private $pageService;
+
+    private $localeService;
+
+    private $localeConfigService;
+
+    private $languageRepository;
+
+    public function __construct(PageService $pageService, LocaleService $localeService, LocaleConfigService $localeConfigService, LanguageRepository $languageRepository = null)
     {
         $this->pageService = $pageService;
+        $this->localeService = $localeService;
+        $this->localeConfigService = $localeConfigService;
+        $this->languageRepository = $languageRepository;
     }
 
     public function getName()
     {
-        return "agit.page.navigation";
+        return "agit.page";
     }
 
     public function getFunctions()
     {
         return [
             new Twig_SimpleFunction("getPageTree", [$this, "getPageTree"]),
+
+            new Twig_SimpleFunction("getPageUrls", [$this, "getPageUrls"]),
+            new Twig_SimpleFunction("getPageLocaleUrls", [$this, "getPageLocaleUrls"], ["needs_context" => true, "is_safe" => ["all"]]),
+
+            new Twig_SimpleFunction("createUrl", [$this, "createUrl"], ["is_safe" => ["all"]]),
             new Twig_SimpleFunction("breadcrumb", [$this, "breadcrumb"],  ["needs_context" => true]),
+
             new Twig_SimpleFunction("hasPrev", [$this, "hasPrev"],  ["needs_context" => true]),
             new Twig_SimpleFunction("hasNext", [$this, "hasNext"],  ["needs_context" => true]),
             new Twig_SimpleFunction("getPrev", [$this, "getPrev"],  ["needs_context" => true]),
@@ -49,6 +68,12 @@ class PageNavigationExtension extends Twig_Extension
         $this->sortTree($tree);
 
         return $tree;
+    }
+
+    public function getPageUrls($base)
+    {
+        $tree = $this->getPageTree($base);
+        return $this->getPages($tree, $this->localeService->getLocale());
     }
 
     public function breadcrumb($context, $base, $withLinks = false)
@@ -168,5 +193,90 @@ class PageNavigationExtension extends Twig_Extension
                 $this->sortTree($branch["children"]);
             }
         }
+    }
+
+    private function getPages(array $tree, $locale)
+    {
+        $pages = [];
+
+        foreach ($tree as $key => $value) {
+            $name = isset($value["data"]["names"][$locale]) ? $value["data"]["names"][$locale] : $value["data"]["name"];
+
+            if (isset($value["children"]) && count($value["children"]))
+            {
+                $pages[$name] = $this->getPages($value["children"], $locale);
+            }
+            elseif (!$value["data"]["isVirtual"])
+            {
+                $pages[$name] = $this->pageService->createUrl($value["data"]["vPath"], $locale);
+            }
+        }
+
+        return $pages;
+    }
+
+    // returns the canonical path of the given path
+    public function createUrl($vPath, $locale = null)
+    {
+        return $this->pageService->createUrl($vPath, $locale ?: $this->localeService->getLocale());
+    }
+
+    public function getPageLocaleUrls($context)
+    {
+        $list = [];
+
+        if (isset($context["localeUrls"]) && $this->languageRepository) {
+            $localeList = $this->localeConfigService->getActiveLocales();
+            $languageCountryMap = [];
+
+            foreach ($localeList as $localeCode) {
+                if (strlen($localeCode) !== 5 || $localeCode[2] !== "_") {
+                    continue;
+                }
+
+                $langCode = substr($localeCode, 0, 2);
+                $countryCode = substr($localeCode, 3);
+
+                if (! isset($languageCountryMap[$langCode])) {
+                    $languageCountryMap[$langCode] = [];
+                }
+
+                $languageCountryMap[$langCode][] = $countryCode;
+            }
+
+            $langCodes = array_map(
+                function ($locale) { return substr($locale, 0, 2); },
+                array_keys($context["localeUrls"])
+            );
+
+            $this->languageRepository->findBy(["id" => $langCodes]); // preloading, to reduce no. of DB queries
+
+            foreach ($context["localeUrls"] as $locale => $url) {
+                $lang = substr($locale, 0, 2);
+                $country = substr($locale, 3);
+                $language = $this->languageRepository->find($lang);
+
+                if ($language) {
+                    $name = $language->getLocalName();
+
+                    if (count($languageCountryMap[$lang]) > 1) {
+                        $name .= " ($country)";
+                    }
+
+                    $list[$locale] = [
+                        "url"       => $url,
+                        "name"      => $name,
+                        "isCurrent" => $locale === $this->localeService->getLocale()
+                    ];
+                }
+            }
+
+            $collator = new Collator($this->localeService->getLocale());
+            usort($list, function ($elem1, $elem2) use ($collator) {
+                return $collator->compare($elem1["name"], $elem2["name"]);
+            });
+        }
+
+        return $list;
     }
 }
